751aaa5f9ba9625ec91919dbe8d4c2048e2fcffe
PuFGGs <35533294+PuFGGs@users.noreply.github.com>
2021-05-17 04:54:49 +0300

    Update player.lua

--- qb-core/server/player.lua
+++ qb-core/server/player.lua
@@ -9,19 +9,8 @@
 			self.Functions.UpdatePlayerData()
 		else
 			if self.PlayerData.money[moneytype] ~= nil then
-				-- if moneytype == 'cash' then
-				-- 	self.Functions.AddItem('cash', amount)
-				-- 	TriggerClientEvent('inventory:client:ItemBox', self.PlayerData.source, QBCore.Shared.Items['cash'], 'add', amount)
-				-- end
 				self.PlayerData.money[moneytype] = self.PlayerData.money[moneytype]+amount
 				self.Functions.UpdatePlayerData()
-				-- TriggerEvent("qb-log:server:sendLog", self.PlayerData.citizenid, "moneyadded", {amount=amount, moneytype=moneytype, newbalance=self.PlayerData.money[moneytype], reason=reason})
-				if amount > 100000 then
-					TriggerEvent("qb-log:server:CreateLog", source,  "playermoney", "AddMoney", "lightgreen",  "$"..amount .. " ("..moneytype..") eklendi, yeni "..moneytype.." parası: "..self.PlayerData.money[moneytype], true)
-				else
-					TriggerEvent("qb-log:server:CreateLog", source,  "playermoney", "AddMoney", "lightgreen", "$"..amount .. " ("..moneytype..") eklendi, yeni "..moneytype.." parası: "..self.PlayerData.money[moneytype])
-				end
-				-- TriggerClientEvent("hud:client:OnMoneyChange", self.PlayerData.source, moneytype, amount, false)
 				return true
 			end
 		end
@@ -33,21 +22,11 @@
 		local moneytype = moneytype:lower()
 		local amount = tonumber(amount)
 		if amount < 0 then return end
-		-- if self.PlayerData.money[moneytype] ~= nil then
-			-- if self.Functions.GetItemByName('cash') ~= nil then
 				if moneytype == 'cash' then
 					if self.Functions.GetItemByName('cash') ~= nil then
 						if self.Functions.GetItemByName('cash').amount >= amount then
 							self.Functions.RemoveItem('cash', amount)
 							self.Functions.UpdatePlayerData()
-							-- TriggerEvent("qb-log:server:sendLog", self.PlayerData.citizenid, "moneyremoved", {amount=amount, moneytype=moneytype, newbalance=self.PlayerData.money[moneytype], reason=reason})
-							if amount > 100000 then
-								TriggerEvent("qb-log:server:CreateLog", source,  "playermoney", "RemoveMoney", "red", "€"..amount .. " ("..moneytype..") silindi, yeni "..moneytype.." bakiyesi: "..self.PlayerData.money[moneytype], true)
-							else
-								TriggerEvent("qb-log:server:CreateLog", source,  "playermoney", "RemoveMoney", "red", "€"..amount .. " ("..moneytype..") silindi, yeni "..moneytype.." bakiyesi: "..self.PlayerData.money[moneytype])
-							end
-							-- TriggerClientEvent("hud:client:OnMoneyChange", self.PlayerData.source, moneytype, amount, true)
-							-- TriggerClientEvent('qb-phone_new:client:RemoveBankMoney', self.PlayerData.source, amount)
 							return true
 						else
 							return false
@@ -64,22 +43,11 @@
 						end
 						self.PlayerData.money[moneytype] = self.PlayerData.money[moneytype] - amount
 						self.Functions.UpdatePlayerData()
-						-- TriggerEvent("qb-log:server:sendLog", self.PlayerData.citizenid, "moneyremoved", {amount=amount, moneytype=moneytype, newbalance=self.PlayerData.money[moneytype], reason=reason})
-						if amount > 100000 then
-							TriggerEvent("qb-log:server:CreateLog", source,  "playermoney", "RemoveMoney", "red", "€"..amount .. " ("..moneytype..") removed, new "..moneytype.." balance: "..self.PlayerData.money[moneytype], true)
-						else
-							TriggerEvent("qb-log:server:CreateLog", source,  "playermoney", "RemoveMoney", "red", "€"..amount .. " ("..moneytype..") removed, new "..moneytype.." balance: "..self.PlayerData.money[moneytype])
-						end
-						-- TriggerClientEvent("hud:client:OnMoneyChange", self.PlayerData.source, moneytype, amount, true)
-						-- TriggerClientEvent('qb-phone_new:client:RemoveBankMoney', self.PlayerData.source, amount)
 						return true
 					else
 						return false
 					end
 				end
-			-- end
-		-- end
-		-- return false
 	end
 
 	self.Functions.SetMoney = function(moneytype, amount, reason)
@@ -102,8 +70,6 @@
 		elseif self.PlayerData.money[moneytype] ~= nil then
 			self.PlayerData.money[moneytype] = amount
 			self.Functions.UpdatePlayerData()
-			-- TriggerEvent("qb-log:server:sendLog", self.PlayerData.citizenid, "moneyset", {amount=amount, moneytype=moneytype, newbalance=self.PlayerData.money[moneytype], reason=reason})
-			TriggerEvent("qb-log:server:CreateLog", source,  "playermoney", "SetMoney", "green", "€"..amount .. " ("..moneytype..") gezet, nieuw "..moneytype.." balans: "..self.PlayerData.money[moneytype])
 			return true
 		end
 		return false
